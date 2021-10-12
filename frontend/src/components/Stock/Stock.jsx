c803e73114072c3ae27420ee20d3c57e4bd3a36b
unknown <conocimiento.02@gmail.com>
2021-10-12 10:29:28 -0500

    primer cambio

--- frontend/src/components/Stock/Stock.jsx
+++ frontend/src/components/Stock/Stock.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import styles from './Stock.module.css';
+import Logo from '../../assets/Logo Merca Todo.png';
+import Cesta from '../../assets/cesta.png';
+import Filtro from '../../assets/Filtro.png';
 const Stock = () => {
     return (
-        <div>
+        <div className={styles.container}>
+            <div className={styles.heder}>
+
+                <img src={Logo} alt="Logo" className={styles.Logo}/> 
+                <input type="search" placeholder="Buscar producto" className={styles.input}/>
+                <img src={Filtro} alt="Filtro" className={styles.Filtro}/>
+
+            </div>
+
             <h1>Hola desde Componente Stock</h1>
+            <div className={styles.cont_Cesta}>
+            <img src={Cesta} alt="Cesta" className={styles.Cesta}/>
+            </div>
         </div>
     )
 }
