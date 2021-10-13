c3cae0e5b1aee6ea195d44338c66155f76cb4865
davidB2ya <dbedoya0408@gmail.com>
2021-10-13 01:31:07 -0500

    Avances del Stock

--- frontend/src/components/Stock/Stock.jsx
+++ frontend/src/components/Stock/Stock.jsx
@@ -1,31 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Stock.module.css';
 import Logo from '../../assets/Logo Merca Todo.png';
-import Cesta from '../../assets/cesta.png';
 import Filtro from '../../assets/Filtro.png';
 import Card from './CardOne/CardOne';
-
+import * as StockServer from './StockServer'
 
 const Stock = () => {
+
+    const [products, setProducts] = useState([]);
+    const [productsMap, setProductsMap] = useState(products);
+
+    const listProducts = async () =>{
+        try{
+            const res = await StockServer.listProducts();
+            const data = await res.json();
+            setProducts(data.productos);
+            setProductsMap(data.productos);
+            console.log(products);
+        }catch(error){
+            console.log(error);
+        }
+    };
+
+    useEffect(() =>{
+        listProducts();
+    },[]);
+
+    const inputChange = (e) => {
+        const search = e.target.value.toLowerCase().trim()
+        if (search === '') {
+            setProductsMap(products)
+        } else {
+          const filter = productsMap.filter(e => e.pro_name.toLowerCase().startsWith(search))
+          setProductsMap(filter)
+        }
+      }
+
     return (
         <div className={styles.container}>
-            <div className={styles.heder}>
-
+            <div className={styles.header}>
                 <img src={Logo} alt="Logo" className={styles.Logo}/> 
-                <input type="search" placeholder="Buscar producto" className={styles.input}/>
+                <input  className={styles.input}
+                    onChange={(e)=> inputChange(e)} 
+                    placeholder={"Buscar producto"} 
+                    type="search"/>
                 <img src={Filtro} alt="Filtro" className={styles.Filtro}/>
-
             </div>
-
-            <h1>Hola desde Componente Stock</h1>
-            <div className={styles.cont_Cesta}>
-            <img src={Cesta} alt="Cesta" className={styles.Cesta}/>
-            </div>
-
-            <div>
-                <Card/>
-            </div>
-            
+            <div className={styles.products}>
+                {productsMap.map(element =>(
+                    <Card key={element.id} data={element}/>
+                ))}
+            </div>            
         </div>
     )
 }
